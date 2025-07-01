c9719efca78fa2c731188eb1ed3b60871a84a5e8
fairmog <fairmog@gmail.com>
2025-07-01 12:37:00 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,10 +2,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Bevelient Admin</title>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body>{children}</body>
+      <body className="antialiased">
+        {children}
+      </body>
     </html>
   )
 }
